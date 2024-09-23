708918c81fa798e55a4d5416e59d150cdde91738
Syed Asad Ali <58474263+SyedAsad-dev@users.noreply.github.com>
2024-09-23 22:42:47 +0400

    🛠️ Navigation Issue on iPhone 16 Pro/Pro Max's (Portrait Mode)
    While working on NavigationStack with iPhone 16 Pro and Pro Max, I encountered an issue where the second screen couldn't navigate back to the Home Screen—it would get stuck every time, but only in portrait mode. 🛑
    
    🔍 After some investigation, I found in Home Screen the culprit:
    
    .navigationTitle("Events")
    .navigationBarTitleDisplayMode(.large)
    
    The fix? Changing .navigationBarTitleDisplayMode(.large) to .inline:
    
    .navigationBarTitleDisplayMode(.inline)

--- EcabTask/Packages/Presentation/EventList/Sources/EventList/Screens/Views/EventListView.swift
+++ EcabTask/Packages/Presentation/EventList/Sources/EventList/Screens/Views/EventListView.swift
@@ -58,7 +58,7 @@ public struct EventListView: View {
            )
         }
         .navigationTitle("Events")
-        .navigationBarTitleDisplayMode(.large)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
